670459a14da77d7a50059f154cfa692514cefcca
IkhtiyorDev <ikhtiyordev@gmail.com>
2021-07-25 13:11:54 +0500

    commit

---
+++ src/main/java/dev/ikhtiyor/springbootfullemail/SpringBootFullEmailApplication.java
@@ -0,0 +1,13 @@
+package dev.ikhtiyor.springbootfullemail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFullEmailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootFullEmailApplication.class, args);
+    }
+
+}
