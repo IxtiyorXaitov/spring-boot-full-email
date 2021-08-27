1a0e9338e3755b23e2fdd9f3b236b66c9c22e769
Ikhtiyor Dev <ikhtiyordev@gmail.com>
2021-08-27 20:29:13 +0500

    commit

--- src/main/java/dev/ikhtiyor/springbootfullemail/service/EmailService.java
+++ src/main/java/dev/ikhtiyor/springbootfullemail/service/EmailService.java
@@ -6,6 +6,7 @@ import freemarker.template.Configuration;
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
@@ -34,6 +35,9 @@ public class EmailService {
         this.config = config;
     }
 
+    @Value("${spring.mail.username}")
+    String fromEmail;
+
     public Response sendEmail(Request request) {
         Map<String, Object> model = new HashMap<>();
         model.put("fullName", request.getFullName());
@@ -49,34 +53,40 @@ public class EmailService {
             return new Response("Sended", true);
 
         } catch (MessagingException | IOException | TemplateException e) {
+            e.printStackTrace();
             return new Response("Error", false);
         }
     }
 
     public void sendEmailWithHtml(Map<String, Object> model, Request request, String htmlFile) throws MessagingException, IOException, TemplateException {
-        MimeMessage message = javaMailSender.createMimeMessage();
-        // set mediaType
-        MimeMessageHelper helper = new MimeMessageHelper(
-                message,
-                MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED,
-                StandardCharsets.UTF_8.name()
-        );
-        // add attachment
-        // helper.addAttachment("logo.png", new ClassPathResource("logo.png"));
-        Template t = config.getTemplate(htmlFile);
-        String html = FreeMarkerTemplateUtils.processTemplateIntoString(t, model);
+        try {
+            MimeMessage message = javaMailSender.createMimeMessage();
+            message.setFrom(fromEmail);
+            // set mediaType
+            MimeMessageHelper helper = new MimeMessageHelper(
+                    message,
+                    MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED,
+                    StandardCharsets.UTF_8.name()
+            );
+            // add attachment
+            // helper.addAttachment("logo.png", new ClassPathResource("logo.png"));
+            Template t = config.getTemplate(htmlFile);
+            String html = FreeMarkerTemplateUtils.processTemplateIntoString(t, model);
 
-        helper.setTo(request.getSendTo());
-        helper.setSubject(request.getSubject());
-        helper.setText(html, true);
-        // helper.setSubject("\uD83D\uDCDB noreply");
-        javaMailSender.send(message);
+            helper.setTo(request.getSendTo());
+            helper.setSubject(request.getSubject());
+            helper.setText(html, true);
+            // helper.setSubject("\uD83D\uDCDB noreply");
+            javaMailSender.send(message);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     public Response sendEmailWithText(Request request) {
         try {
             SimpleMailMessage msg = new SimpleMailMessage();
-
+            msg.setFrom(fromEmail);
             msg.setTo(request.getSendTo());
             msg.setSubject(request.getSubject());
             msg.setText(request.getMessage());
@@ -84,6 +94,7 @@ public class EmailService {
             javaMailSender.send(msg);
             return new Response("Sended", true);
         } catch (Exception e) {
+            e.printStackTrace();
             return new Response("Error", false);
         }
     }
@@ -92,7 +103,7 @@ public class EmailService {
         try {
 
             MimeMessage msg = javaMailSender.createMimeMessage();
-
+            msg.setFrom(fromEmail);
             // true = multipart message
             MimeMessageHelper helper = new MimeMessageHelper(msg, true);
             helper.setTo(request.getSendTo());
